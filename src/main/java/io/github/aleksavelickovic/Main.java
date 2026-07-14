28887103bbd18316d38d87418d537f523f2c5ef3
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2026-07-14 22:53:20 +0200

    [REFAC] Removed exceptions that were never thrown in Main.java

--- src/main/java/io/github/aleksavelickovic/Main.java
+++ src/main/java/io/github/aleksavelickovic/Main.java
@@ -7,12 +7,11 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+    static void main(String[] args) throws IOException {
 
         List options = getOptionsList();
 
